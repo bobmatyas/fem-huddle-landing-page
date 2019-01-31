133da9987aa9b4bf49312edb42950e3854156556
bobmatyas <ballium@protonmail.com>
2019-01-31 11:50:32 -0500

    remove unused javascript

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("email-input").addEventListener("focus", function(){
-  document.getElementById("#email-label").style.display = 'none';
-});
-
-document.getElementById("#email").addEventListener("blur", function(){
-  document.getElementById("#email-label").style.display = 'inline';
-});
